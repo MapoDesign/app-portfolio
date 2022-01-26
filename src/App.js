b766a787d12ab377378d0acef83d0ad5f7e9fbbd
MapoDesign <elisaresmini@gmail.com>
2022-01-26 17:44:48 +0100

    basename router

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
      
-      <BrowserRouter />
+      <BrowserRouter basename = 'https://mapodesign-portfolio.netlify.app'/>
     </div>
   );
 }
