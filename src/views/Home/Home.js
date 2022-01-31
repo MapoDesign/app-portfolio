3e01e680459a607f7a83b13a6bf40fa8729dd3bf
MapoDesign <elisaresmini@gmail.com>
2022-01-31 17:34:45 +0100

    yellow slide

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,14 +1,13 @@
 
-import { Link } from "react-router-dom";
 import '../../App.css';
 
 export default function Home(){
     return <>
         <header className="App-header">
-            <Link to="/list" id="React" class="sidebar"><h1>React</h1></Link>
-            <Link to="https://mapodesign.github.io/libreria" id="JS" class="sidebar"><h1>JavaScript Vanilla</h1></Link>
-            <Link to="https://www.mapodesign.com/category/blog/portfolio/wordpress" id="WordPress" class="sidebar"><h1>WordPress</h1></Link>
-            <Link to="https://scambiaorto.it" id="eComm" class="sidebar"><h1>eCommerce</h1></Link>
+            <h1 id="React" class="sidebar">React</h1>
+            <h1 id="JS" class="sidebar">JavaScript Vanilla</h1>
+            <h1 id="WordPress" class="sidebar">WordPress</h1>
+            <h1 id="eComm" class="sidebar">eCommerce</h1>
         {/* <Image src="https://images.pexels.com/photos/8658345/pexels-photo-8658345.jpeg" fluid />*/}
         </header>
     </>
