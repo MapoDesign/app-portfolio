278bbbe5846a923108dd87cb29feb7167b9a00b9
MapoDesign <elisaresmini@gmail.com>
2022-01-31 14:58:30 +0100

    useEffect

--- src/Components/ClientAPI/FetchProduct.js
+++ src/Components/ClientAPI/FetchProduct.js
@@ -1,4 +1,8 @@
+import { useEffect } from "react";
+import useProductAPI from "./useProductAPI";
+
 export default function FetchProduct(){
+    const productClient = useProductAPI();
     useEffect(()=>{productClient.fetchProduct("m")},[]);
 
 }
