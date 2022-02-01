74bd231ad9d966fdd97afaab109a22f3b240f5ac
MapoDesign <elisaresmini@gmail.com>
2022-02-01 19:42:52 +0100

    no consolelog API

--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -9,7 +9,7 @@ function News(){
     async function getUser() {
         try {
           const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=c0c5fcd80fb14c87b72c496cf4c87522');
-          console.log(response);
+          
           setData(response.data.articles);
         } catch (error) {
           console.error(error);
