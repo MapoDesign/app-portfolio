ad655e6e9f158819cd37c6a4d715e6e9c5c39cdb
MapoDesign <elisaresmini@gmail.com>
2022-02-03 11:18:29 +0100

    git reset prova

---
+++ src/Components/News/Filter.js
@@ -0,0 +1,14 @@
+
+import Select from 'react-select';
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+]
+
+const Filter = () => (
+    <Select options={options} />
+  )
+
+export default Filter;
