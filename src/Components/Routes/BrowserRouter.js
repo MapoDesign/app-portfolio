4efa615ec01c572452de642e3aafb033a77854db
MapoDesign <elisaresmini@gmail.com>
2022-12-12 16:14:23 +0100

    footer

--- src/Components/Routes/BrowserRouter.js
+++ src/Components/Routes/BrowserRouter.js
@@ -1,21 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../../views/Home/Home";
+import List from "../../views/List/List";
+import LogIn from "../../views/Login/Login";
+import Curriculum from "../../views/Curriculum/Curriculum";
+import NavBar from "../Header/NavBar/NavBar";
+import Footer from "../Footer/Footer";
 
-import {BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from '../../views/Home/Home'
-import List from '../../views/List/List'
-import LogIn from '../../views/Login/Login'
-import Curriculum from '../../views/Curriculum/Curriculum'
-import NavBar from '../Header/NavBar/NavBar'
-
-
-export default function CustomRutes(){
-    return <BrowserRouter >
-        <NavBar/>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/curriculum" element={<Curriculum />} />
-            <Route path="/list" element={<List />} />
-            <Route path="/login" element={<LogIn />} />
-            <Route path="*" element={<div>Not found</div>} />
-        </Routes>
+export default function CustomRutes() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/curriculum" element={<Curriculum />} />
+        <Route path="/list" element={<List />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="*" element={<div>Not found</div>} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
+  );
 }
