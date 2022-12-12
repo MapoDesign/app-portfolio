fe8f3282587a54894db94e336d7f77d0fa94ebd7
MapoDesign <elisaresmini@gmail.com>
2022-12-12 15:57:45 +0100

    hard skills

--- src/views/Curriculum/Curriculum.js
+++ src/views/Curriculum/Curriculum.js
@@ -1,59 +1,160 @@
 import { Container, Row } from "react-bootstrap";
-import '../../Components/Products/ProductCard.css';
+import "../../Components/Products/ProductCard.css";
 
-export default function List(){
-    return <>
-        <header id="slider" className="App-header">
-            <div class="text-content">
-                <h3>Benvenuto! Io sono</h3>
-                <h1>Front-end Developer</h1>
+export default function List() {
+  return (
+    <>
+      <Container>
+        <Row>
+          {/* Section Title  */}
+          <div className="title-wrap mb-5" data-aos="fade-up">
+            <h2 className="section-title">Curriculum Vitae</h2>
+            <p className="section-sub-title">
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur
+              et. <br /> pharetra augue. Donec id elit non mi.
+            </p>
+          </div>
+          {/* End of Section Title  */}
+        </Row>
+
+        <Row>
+          {/* Section Title  */}
+          <div className="title-wrap mb-5" data-aos="fade-up">
+            <h2 className="section-title">Hard Skills</h2>
+          </div>
+          {/* End of Section Title  */}
+
+          {/* Skills */}
+          <div className="col-md-10 offset-md-1 features-holder">
+            <div className="row">
+              {/* Skills Item */}
+              <div className="col-md-4 col-sm-12 text-center mt-4">
+                <div
+                  className="shadow rounded feature-item p-4 mb-4"
+                  data-aos="fade-up"
+                >
+                  <div className="my-4">
+                    <i className="lnr lnr-cog fs-40"></i>
+                  </div>
+                  <h4>Front-end Developer</h4>
+                  <p>
+                    Creazione di siti e app con JavaScript, React e JQuery.
+                    Lavoro in team con GIT da powershell, e preferibilmente
+                    GitLab.
+                  </p>
+                </div>
+              </div>
+              {/* End of Skills Item */}
+              {/* Skills Item */}
+              <div className="col-md-4 col-sm-12 text-center mt-4">
+                <div
+                  className="shadow rounded feature-item p-4 mb-4"
+                  data-aos="fade-up"
+                >
+                  <div className="my-4">
+                    <i className="lnr lnr-cog fs-40"></i>
+                  </div>
+                  <h4>Custom templates</h4>
+                  <p>
+                    Template personalizzati HTML5 + CSS3 per WordPress,
+                    newsletter e landing pages.
+                  </p>
+                </div>
+              </div>
+              {/* End of Skills Item */}
+              {/* Skills Item */}
+              <div className="col-md-4 col-sm-12 text-center mt-4">
+                <div
+                  className="shadow rounded feature-item p-4 mb-4"
+                  data-aos="fade-up"
+                >
+                  <div className="my-4">
+                    <i className="lnr lnr-cog fs-40"></i>
+                  </div>
+                  <h4>Newsletter Developer</h4>
+                  <p>
+                    Posso gestire le newsletter a livello UX + UI e developer.
+                    Creo la struttura di newsletter responive, animate e con
+                    struttura personalizzata.
+                  </p>
+                </div>
+              </div>
+              {/* End of Skills Item */}
+
+              {/* Skills Item */}
+              <div className="col-md-4 col-sm-12 text-center mt-4">
+                <div
+                  className="shadow rounded feature-item p-4 mb-4"
+                  data-aos="fade-up"
+                >
+                  <div className="my-4">
+                    <i className="lnr lnr-cog fs-40"></i>
+                  </div>
+                  <h4>Smart Work</h4>
+                  <p>
+                    Ho esperienza di AGILE SCRUM in team Vodafone e Smart Work
+                    Leroy Merlin. Tool: JIRA - Slack - Trello.
+                  </p>
+                </div>
+              </div>
+              {/* End of Skills Item */}
+              {/* Skills Item */}
+              <div className="col-md-4 col-sm-12 text-center mt-4">
+                <div
+                  className="shadow rounded feature-item p-4 mb-4"
+                  data-aos="fade-up"
+                >
+                  <div className="my-4">
+                    <i className="lnr lnr-cog fs-40"></i>
+                  </div>
+                  <h4>Bootstrap</h4>
+                  <p>
+                    Ho conoscenze di Bootstrap 5 e precedenti, Bootstrap Italia
+                    (per pubblica amministrazione).
+                  </p>
+                </div>
+              </div>
+              {/* End of Skills Item */}
+              {/* Skills Item */}
+              <div className="col-md-4 col-sm-12 text-center mt-4">
+                <div
+                  className="shadow rounded feature-item p-4 mb-4"
+                  data-aos="fade-up"
+                >
+                  <div className="my-4">
+                    <i className="lnr lnr-cog fs-40"></i>
+                  </div>
+                  <h4>Gestione CMS</h4>
+                  <p>
+                    Posso progettare ed installare un sito WordPress. Ho gestito
+                    siti eCommerce in Prestashop e Magento.
+                  </p>
+                </div>
+              </div>
+              {/* End of Skills Item */}
+              {/* Skills Item */}
+              <div className="col-md-4 col-sm-12 text-center mt-4">
+                <div
+                  className="shadow rounded feature-item p-4 mb-4"
+                  data-aos="fade-up"
+                >
+                  <div className="my-4">
+                    <i className="lnr lnr-cog fs-40"></i>
+                  </div>
+                  <h4>UX / UI Siti Web</h4>
+                  <p>
+                    Ti serve strutturare il tuo sito sencondo la visione di User
+                    Experience o una grafica personalizzata del tuo sito? Posso
+                    farlo!
+                  </p>
+                </div>
+              </div>
+              {/* End of Skills Item */}
             </div>
-        </header>
-        <Container>
-            <Row>
-                <h1>Curriculum</h1>
-            </Row>
-        
-            <Row>
-                <h2>Hard Skills</h2>
-                <div className="product-card">
-                    <h3>Front-end Developer</h3>
-                    <p>Creazione di siti e app con JavaScript, React e JQuery. Lavoro in team con GIT da powershell, e preferibilmente GitLab.</p>
-                </div>
-                
-                <div className="product-card">
-                    <h3>Custom templates</h3>
-                    <p>Template personalizzati HTML5 + CSS3 per WordPress, newsletter e landing pages.</p>
-                </div>
-
-                <div className="product-card">
-                    <h3>Newsletter Developer</h3>
-                    <p>Posso gestire le newsletter a livello UX + UI e developer. Creo la struttura di newsletter responive, animate e con struttura personalizzata.</p>
-                </div>
-                <div className="product-card">
-                    <h3>Smart Work</h3>
-                    <p>Ho esperienza di AGILE SCRUM in team Vodafone e Smart Work Leroy Merlin. Tool: JIRA - Slack - Trello.</p>
-                </div>
-                <div className="product-card">
-                    <h3>Bootstrap</h3>
-                    <p>Ho conoscenze di Bootstrap e Bootstrap Italia (per pubblica amministrazione), questo sito ad esempio è creato così.</p>
-                </div>
-                <div className="product-card">
-                    <h3>Gestione CMS</h3>
-                    <p>Posso progettare ed installare un sito WordPress. Ho gestito siti eCommerce in Prestashop e Magento. Drupal COMING SOON!</p>
-                </div>
-                <div className="product-card">
-                    <h3>UX / UI Siti Web</h3>
-                    <p>Ti serve una grafica personalizzata del tuo sito? Posso farlo!</p>
-                </div>
-                <div className="product-card">
-                    <h3>Adobe CC</h3>
-                    <p>Photoshop, Illustrator, Indesign, Premiere, XD. In possesso di utenza personale di Adobe.</p>
-                </div>
-            </Row>
-
-        
-    </Container>
+          </div>
+          {/* End of Skills */}
+        </Row>
+      </Container>
     </>
-    
+  );
 }
