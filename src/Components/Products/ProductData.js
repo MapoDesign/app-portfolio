1f9e9a5098ab25b2ade160c1baf374f7c313b300
MapoDesign <elisaresmini@gmail.com>
2023-01-05 16:02:58 +0100

    style list

--- src/Components/Products/ProductData.js
+++ src/Components/Products/ProductData.js
@@ -1,10 +1,26 @@
-import './ProductCard.css'
+import "./ProductCard.css";
 
 const ProductData = (props) => {
-    return <div className='product-card'>
-        <p className='title'>{props.drink.strDrink}</p>
-        <img src={props.drink.strDrinkThumb} alt="" height="200" width="200" />
-    </div>
-}
+  return (
+    <div class="card bg-dark text-white">
+      <img
+        className="card-img"
+        src={props.drink.strDrinkThumb}
+        alt=""
+        height="200"
+        width="200"
+      />
 
-export default ProductData
+      <div class="card-img-overlay">
+        <h5 class="card-title">{props.drink.strDrink}</h5>
+        <p class="card-text">
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
+        <p class="card-text">Last updated 3 mins ago</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductData;
